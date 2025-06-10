2aa8b639c1a5512f21c4b374659bb465bcc497a5
PrincessCS <princeschidera@gmail.com>
2025-06-10 16:46:09 +0100

    added advice generator

---
+++ advice-generator-app-main/script.js
@@ -0,0 +1,18 @@
+const button = document.getElementById("advice-btn");
+const advice = document.getElementById("advice");
+const adviceNumber = document.getElementById("advice-number");
+function getAdvice(){
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => response.json())
+    .then(data => {
+        adviceNumber.textContent = `#${data.slip.id}`;
+        advice.textContent = `"${data.slip.advice}"`;
+        document.title = `Advice #${data.slip.id}`;
+    }   
+    )
+    .catch(err => {
+      advice.textContent = "Sorry, we couldn't load advice. Please try again.";
+      console.error("Error loading data:", err);
+    });
+}
+button.addEventListener("click", getAdvice);
